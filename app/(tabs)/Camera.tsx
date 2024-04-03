44d16dc320e0d3b66c1ed577a5ffa0960365b142
Gracie0119 <gracie.ray12121@gmail.com>
2024-04-03 17:15:29 -0500

    added cameraish
    added components but still not functioning right

--- app/(tabs)/Camera.tsx
+++ app/(tabs)/Camera.tsx
@@ -1,15 +1,80 @@
 import { YStack, H2, Separator, Theme } from 'tamagui';
-
 import EditScreenInfo from '../../components/edit-screen-info';
+import { Camera, CameraType } from 'expo-camera';
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function TabCameraScreen() {
+export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+               <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function TabCameraScreen() {
+    return (
+      <Theme name="light">
+        <YStack flex={1} alignItems="center" justifyContent="center">
+          <H2>Camera</H2>
+          <Separator />
+          <EditScreenInfo path="app/(tabs)/index.tsx" />
+        </YStack>
+      </Theme>
+    );
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
   return (
-    <Theme name="light">
-      <YStack flex={1} alignItems="center" justifyContent="center">
-        <H2>Camera</H2>
-        <Separator />
-        <EditScreenInfo path="app/(tabs)/index.tsx" />
-      </YStack>
-    </Theme>
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
+
